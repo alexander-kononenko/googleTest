3f5988b492f78e1417c53bdb575ab56fd14fd49d
alexander-kononenko <kononenko192@gmail.com>
2018-07-25 17:06:34 +0300

    Create HomePage.java

---
+++ HomePage.java
@@ -0,0 +1,85 @@
+package pages;
+
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class HomePage {
+    Timestamp timestamp;
+
+    public HomePage (WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+    private  WebDriver driver;
+
+    //Waiting for "write email" button than click it
+    @FindBy(xpath ="//div[@class='T-I J-J5-Ji T-I-KE L3']")
+    public  WebElement wrEmailBtn;
+
+    public  void wrEmail() {
+        WebDriverWait waitForPass = new WebDriverWait(driver, 10);
+        waitForPass.until(ExpectedConditions.elementToBeClickable(wrEmailBtn));
+        }
+
+
+    //Sending some emails
+    public  void sendEmails(){
+        for ( int i = 0 ; i < 4; i++) {
+        timestamp = new Timestamp(System.currentTimeMillis());
+        wrEmailBtn.click();
+        WebDriverWait waitForEmailForm = new WebDriverWait(driver, 10);
+        waitForEmailForm.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@name='subjectbox']")));
+        WebElement wrEmailTo = driver.findElement(By.xpath("//textarea[@name='to']"));
+        wrEmailTo.sendKeys("zalupatest111@gmail.com");
+        WebElement wrEmailTheme = driver.findElement(By.xpath("//input[@name='subjectbox']"));
+        if (i==2)
+            wrEmailTheme.sendKeys("zalupa");
+        else
+            wrEmailTheme.sendKeys("wawa"+timestamp.getTime());
+
+        WebElement wrEmailBody = driver.findElement(By.xpath("//div[@role='textbox']"));
+        wrEmailBody.sendKeys("KerHemHanaycty7@df");
+
+        WebElement wrEmailSnd = driver.findElement(By.xpath("//div[@class='T-I J-J5-Ji aoO T-I-atl L3']"));
+        wrEmailSnd.click();
+    }
+    }
+    //Click Incoming email
+    @FindBy (xpath = "//a[@class='J-Ke n0']")
+    public WebElement incEmail;
+    public void incEmailClick(){
+        incEmail.click();
+    }
+
+
+    //Check if zalupa was send
+    public void zalupaCheck(){
+    List<WebElement> mailList = driver.findElements(By.xpath("//span[@class='bog']/b"));
+        int zalupaFound = 0;
+        for (WebElement row: mailList) {
+        if (row.getText().equals("zalupa")) {
+            zalupaFound += 1;
+        }
+    }
+
+          Assert.assertEquals(zalupaFound, 1); //zalupaFound == 1;
+        System.out.println(zalupaFound);
+
+
+}
+
+
+}
+
+
+
