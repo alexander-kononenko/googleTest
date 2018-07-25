773f3df3155bfcf9816f68ab9a94eeda283496cb
alexander-kononenko <kononenko192@gmail.com>
2018-07-25 17:05:22 +0300

    Create FirstTest.java

---
+++ FirstTest.java
@@ -0,0 +1,50 @@
+package test1;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
+import pages.LoginPage;
+
+import java.sql.Timestamp;
+import java.util.concurrent.TimeUnit;
+
+
+public class FirstTest {
+
+    public static WebDriver driver;
+    public static LoginPage loginPage;
+    public static HomePage homePage;
+
+    Timestamp timestamp;
+
+       @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com/intl/ru/gmail/about/#");
+       }
+
+
+    @Test
+    public void userLogin() {
+      loginPage.loginInput();
+      loginPage.passInput();
+      homePage.wrEmail();
+      homePage.sendEmails();
+      homePage.incEmailClick();
+      homePage.zalupaCheck();
+      }
+
+    @AfterClass
+       public static void tearDown() {
+       driver.quit();
+    }
+}
+
