30452177b25263cedd2c145fa57e1d2781966ba8
alexander-kononenko <kononenko192@gmail.com>
2018-07-26 16:57:56 +0300

    Update FirstTest.java

--- FirstTest.java
+++ FirstTest.java
@@ -14,46 +14,45 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
 
-    public static WebDriver driver;
-    public static LoginPage loginPage;
-    public static HomePage homePage;
+public static WebDriver driver;
+public static LoginPage loginPage;
+public static HomePage homePage;
+Timestamp timestamp;
 
-    Timestamp timestamp;
-
-    @BeforeClass
-    public static void setup() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-        driver = new ChromeDriver();
-        loginPage = new LoginPage(driver);
-        homePage = new HomePage(driver);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.google.com/intl/ru/gmail/about/#");
-    }
+@BeforeClass
+public static void setup() {
+    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+    driver = new ChromeDriver();
+    loginPage = new LoginPage(driver);
+    homePage = new HomePage(driver);
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.get("https://www.google.com/intl/ru/gmail/about/#");
+}
 
 
-    @Test
-    public void userLogin() {
-      loginPage.loginInput();
-      loginPage.passInput();
-      homePage.wrEmail();
-      homePage.sendEmails();
-      homePage.incEmailClick();
-        //Check if zalupa was send
-      List<WebElement> mailList = driver.findElements(By.xpath("//span[@class='bog']/b"));
-        int zalupaFound = 0;
-        for (WebElement row: mailList) {
-            if (row.getText().equals("zalupa")) {
+@Test
+public void userLogin() {
+    loginPage.loginInput();
+    loginPage.passInput();
+    homePage.wrEmail();
+    homePage.sendEmails();
+    homePage.incEmailClick();
+//Check if zalupa was send
+    List<WebElement> mailList = driver.findElements(By.xpath("//span[@class='bog']/b"));
+    int zalupaFound = 0;
+    for (WebElement row: mailList) {
+        if (row.getText().equals("zalupa")) {
                 zalupaFound += 1;
-            }
-      }
-
-      Assert.assertEquals(zalupaFound, 1); 
+        }
     }
 
-    @AfterClass
-    public static void tearDown() {
-      driver.quit();
+    Assert.assertEquals(zalupaFound, 1); 
+}
+
+@AfterClass
+public static void tearDown() {
+    driver.quit();
     }
 }
 
